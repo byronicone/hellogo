31bd7393bd05dc35dd6b2ca29c5809186e4f63be
byronicone <byron.e.barnes@gmail.com>
2018-04-25 15:48:53 -0400

    User Defined Types replace classes

--- hellogo.go
+++ hellogo.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
+type Person struct {
+	name string
+	age  int8
+}
+
 func main() {
 	message, closing := "hello, mars", "i bid you adieu"
 	fmt.Println(message, closing)
 
 	var pointer *string = &message
-	fmt.Println(pointer)
+	fmt.Println(pointer, *pointer)
+
+	var dude = Person{"Byron", 29}
+	fmt.Println(dude, dude.name, dude.age)
+
 }
