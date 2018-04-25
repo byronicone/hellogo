457d2a5d60264426d80d34ea04c453af061e7d0f
byronicone <byron.e.barnes@gmail.com>
2018-04-25 16:06:44 -0400

    defining const in block and use of iota

--- hellogo.go
+++ hellogo.go
@@ -5,7 +5,13 @@ import "fmt"
 type Person struct {
 	name string
 	age  int8
+	id   int
 }
+
+const (
+	A = iota
+	B
+)
 
 func main() {
 	message, closing := "hello, mars", "i bid you adieu"
@@ -14,7 +20,9 @@ func main() {
 	var pointer *string = &message
 	fmt.Println(pointer, *pointer)
 
-	var dude = Person{"Byron", 29}
-	fmt.Println(dude, dude.name, dude.age)
+	var dude = Person{"Byron", 29, A}
+	var lass = Person{"Jane", 47, B}
+	fmt.Println(dude, dude.name, dude.age, dude.id)
+	fmt.Println(lass, lass.name, lass.age, lass.id)
 
 }
