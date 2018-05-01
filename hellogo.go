ab6d91d63e5f690e89dfc71a660615fc16f7441a
byronicone <byron.e.barnes@gmail.com>
2018-05-01 13:50:22 -0400

    funcs as params with closures

--- hellogo.go
+++ hellogo.go
@@ -6,9 +6,10 @@ import (
 )
 
 type Person struct {
-	name string
-	age  int
-	id   int
+	name      string
+	nicknames []string
+	age       int
+	id        int
 }
 
 const (
@@ -16,34 +17,52 @@ const (
 	B
 )
 
-func describe(person Person, nicknames ...string) int {
-	description, _ := createMessage(person.name, person.age)
-	fmt.Println(description)
-
-	numNicks := len(nicknames)
-	if numNicks > 0 {
-		fmt.Printf("Also known as")
-		for i := 0; i < numNicks; i++ {
-			fmt.Printf(" " + nicknames[i])
-			if i != numNicks-1 {
-				fmt.Printf(",")
-			}
-		}
-	}
-	return person.id
-}
-
-func createMessage(name string, age int) (message string, alternate string) {
-	strAge := strconv.Itoa(age)
-	nextAge := strconv.Itoa(age + 1)
-	message = name + " is " + strAge + " years old!"
-	alternate = name + " is going to be " + nextAge + " next year.  How ancient..."
-	return
-}
+type Printer func(Person) int
 
 func main() {
+	var dude = Person{"Byron", []string{"Bigwig", "The Cleaner"}, 29, A}
+	fmt.Println("Simple func:")
+	describe(dude, simpleMessageFunc(dude))
+	fmt.Println("\nVerbose func:")
+	describe(dude, verboseMessageFunc(dude))
+}
 
-	var dude = Person{"Byron", 29, A}
-	_ = describe(dude, "Bigwig", "The Cleaner")
+func describe(person Person, do Printer) {
+	_ = do(person)
+}
 
+func printNameAndAge(name string, age int) {
+	strAge := strconv.Itoa(age)
+	fmt.Println(name + " is " + strAge + " years old!")
+}
+
+func simpleMessageFunc(person Person) (p Printer) {
+	return func(person Person) (id int) {
+		printNameAndAge(person.name, person.age)
+		return person.id
+	}
+}
+
+func verboseMessageFunc(person Person) (p Printer) {
+	return func(person Person) (id int) {
+		name := person.name
+		age := person.age
+		printNameAndAge(person.name, person.age)
+		nextAge := strconv.Itoa(age + 1)
+		fmt.Println(name + " is going to be " + nextAge + " next year.  How ancient...")
+		nicknames := person.nicknames
+		numNicks := len(nicknames)
+		if numNicks > 0 {
+			fmt.Printf("Also known as")
+			for i := 0; i < numNicks; i++ {
+				fmt.Printf(" " + nicknames[i])
+				if i != numNicks-1 {
+					fmt.Printf(",")
+				} else {
+					fmt.Println()
+				}
+			}
+		}
+		return person.id
+	}
 }
