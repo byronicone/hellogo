54316c395a332f35352598e9bf9b730283a9b7a9
byronicone <byron.e.barnes@gmail.com>
2018-05-01 16:49:29 -0400

    switch statement and modularization

--- hellogo.go
+++ hellogo.go
@@ -1,23 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
-type Person struct {
-	name      string
-	nicknames []string
-	age       int
-	id        int
-}
+import "fmt"
 
 const (
 	A = iota
 	B
 )
-
-type Printer func(Person) int
 
 func main() {
 	var dude = Person{"Byron", []string{"Bigwig", "The Cleaner"}, 29, A}
@@ -25,44 +13,4 @@ func main() {
 	describe(dude, simpleMessageFunc(dude))
 	fmt.Println("\nVerbose func:")
 	describe(dude, verboseMessageFunc(dude))
-}
-
-func describe(person Person, do Printer) {
-	_ = do(person)
-}
-
-func printNameAndAge(name string, age int) {
-	strAge := strconv.Itoa(age)
-	fmt.Println(name + " is " + strAge + " years old!")
-}
-
-func simpleMessageFunc(person Person) (p Printer) {
-	return func(person Person) (id int) {
-		printNameAndAge(person.name, person.age)
-		return person.id
-	}
-}
-
-func verboseMessageFunc(person Person) (p Printer) {
-	return func(person Person) (id int) {
-		name := person.name
-		age := person.age
-		printNameAndAge(person.name, person.age)
-		nextAge := strconv.Itoa(age + 1)
-		fmt.Println(name + " is going to be " + nextAge + " next year.  How ancient...")
-		nicknames := person.nicknames
-		numNicks := len(nicknames)
-		if numNicks > 0 {
-			fmt.Printf("Also known as")
-			for i := 0; i < numNicks; i++ {
-				fmt.Printf(" " + nicknames[i])
-				if i != numNicks-1 {
-					fmt.Printf(",")
-				} else {
-					fmt.Println()
-				}
-			}
-		}
-		return person.id
-	}
 }
